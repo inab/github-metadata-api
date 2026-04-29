151b4399dbc6177dae13d1cdfc71b22cea5076c9
Eva Martin del Pico <eva.mdpico@gmail.com>
2026-04-29 13:26:30 +0200

    Update swaggerDefinition.js

--- src/swaggerDefinition.js
+++ src/swaggerDefinition.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 const swaggerJsdoc = require('swagger-jsdoc');
 
 // Read version from the VERSION file in the parent directory
-const version = fs.readFileSync('./VERSION', 'utf8').trim();
+// const version = fs.readFileSync('./VERSION', 'utf8').trim();
 
 const options = {
   swaggerDefinition: {
     openapi: '3.0.0',
     info: {
       title: 'GitHub Metadata API',
-      version: version, // Use the version read from the file
+      // version: version, // Use the version read from the file
     },
   },
   apis: ['./src/metadataExtractor/routes.js', './src/metadataUpdater/routes.js'],
